4f2bc6feb9ab562760eca89b4145c45cec6d3f16
Mkhetyeva <127616754+Mkhetyeva@users.noreply.github.com>
2026-03-08 06:15:28 +0200

    Added calculator functions

--- calculator.py
+++ calculator.py
@@ -9,3 +9,15 @@ choice = input("Choose operation (1/2/3/4): ")
 
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
